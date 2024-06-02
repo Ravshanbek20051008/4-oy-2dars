e9b6ae1ecd4b307058917f25edebf102879e5ded
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-06-02 17:28:00 +0500

    yakunlandi

---
+++ js/index.js
@@ -0,0 +1,81 @@
+// let a = +prompt("3xonali son kiriting");
+// let y = Math.trunc(a / 100);
+// console.log(y);
+// 10-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// console.log(bir + onlar + yuzlar);
+// 11-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(bir + onlar + yuzlar);
+// 12-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(bir * 100 + onlar * 10 + yuzlar);
+// 13-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(onlar * 100 + bir * 10 + yuzlar);
+// 2-Rasm
+// 14-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(bir * 100 + yuzlar * 10 + onlar);
+// 15-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(onlar * 100 + yuzlar * 10 + bir);
+// 16-masala
+// let a = +prompt("3xonali son kiriting");
+// let bir = a % 10;
+// let onlar = Math.trunc((a / 10) % 10);
+// let yuzlar = Math.trunc(a / 100);
+// console.log(yuzlar * 100 + bir * 10 + onlar);
+// 17-masala
+// let a = +prompt("4xonali son kiriting qitmirlik qimaaa");
+// let onlar = Math.trunc((a / 10) % 10);
+// console.log(onlar);
+// 18-masala
+// let a = +prompt("4xonali son kiriting qitmirlik qimaaa");
+// let yuzlar = Math.trunc((a / 100) % 10);
+// console.log(yuzlar);
+// 19-masala
+// let sekund = +prompt("sekund kiriting");
+// let daqiqa = Math.trunc(sekund / 60);
+// console.log(daqiqa);
+// 20-masala
+// let sekund = +prompt("sekund kiriting");
+// let soat = Math.trunc(sekund / 3600);
+// console.log(soat);
+// 21-masala
+// let sekund = +prompt("sekund kiriting");
+// let sekundlar = sekund;
+// let daqiqa = Math.trunc(sekund / 60);
+// console.log("sekund " + sekund);
+// console.log("daqiqa " + daqiqa);
+// 22-masala
+// let sekund = +prompt("sekund kiriting");
+// let sekundlar = sekund;
+// let soat = Math.trunc(sekund / 3600);
+// console.log("sekund " + sekund);
+// console.log("soat " + soat);
+// 23-masala
+// let sekund = +prompt("sekund kiriting");
+// let sekundlar = sekund;
+// let daqiqa = Math.trunc(sekund / 60);
+// let soat = Math.trunc(sekund / 3600);
+// console.log("soat " + soat);
+// console.log("daqiqa " + daqiqa);
+// console.log("sekund " + sekund);
